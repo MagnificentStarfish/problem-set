a3242bbe160d6ff437c96d0e35e9aec6baa9cfc2
donald.powell <donald.powell@galvanize.com>
2023-09-05 22:09:47 -0600

    hoisted functions

--- stuff.js
+++ stuff.js
@@ -440,36 +440,50 @@
 // decrypt(encryptedText, n)
 
 
-function queueTime(customers, n) {
-  console.log("queueTime function is being executed.");
+// function queueTime(customers, n) {
+//   console.log("queueTime function is being executed.");
 
-  try {
-    if (n === 0) {
-      console.log("Tills are down!!");
-      return "Tills are down!!";
-    }
-    if (n === 1) {
-      const total = customers.reduce((sum, time) => sum + time, 0);
-      console.log("Total time for one till:", total);
-      return total;
-    } else {
-      const tills = Array(n).fill(0);
-      for (const time of customers) {
-        const minTill = tills.indexOf(Math.min(...tills));
-        tills[minTill] += time;
-      }
+//   try {
+//     if (n === 0) {
+//       console.log("Tills are down!!");
+//       return "Tills are down!!";
+//     }
+//     if (n === 1) {
+//       const total = customers.reduce((sum, time) => sum + time, 0);
+//       console.log("Total time for one till:", total);
+//       return total;
+//     } else {
+//       const tills = Array(n).fill(0);
+//       for (const time of customers) {
+//         const minTill = tills.indexOf(Math.min(...tills));
+//         tills[minTill] += time;
+//       }
 
-      const result = tills.reduce((sum, time) => sum + time, 0);
-      console.log("Total time for all tills:", result);
-      return result;
-    }
-  } catch (error) {
-    console.error("An error occurred:", error);
-    return "An error occurred.";
+//       const result = tills.reduce((sum, time) => sum + time, 0);
+//       console.log("Total time for all tills:", result);
+//       return result;
+//     }
+//   } catch (error) {
+//     console.error("An error occurred:", error);
+//     return "An error occurred.";
+//   }
+// }
+
+// let customers = [1, 2, 3, 4, 100, 400, 555555, 4125, 124, 12, 3, 5];
+// let n = 4;
+
+// queueTime(customers, n);
+
+
+function foo() {
+  return bar();
+  function bar() {
+  return 5;
   }
-}
+  function bar() {
+  return 10;
+  }
+  }
+  console.log(foo());
 
-let customers = [1, 2, 3, 4, 100, 400, 555555, 4125, 124, 12, 3, 5];
-let n = 4;
-
-queueTime(customers, n);
+  foo()
