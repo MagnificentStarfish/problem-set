3abfd52e84d939b9f0d7a0608e52b1e1283c6bfe
Donald Powell <djp0242@gmail.com>
2023-09-11 14:03:12 -0600

    array work

--- stuff.js
+++ stuff.js
@@ -562,3 +562,29 @@
 
 // let iterable = [1,2,2,3,4,4,4,5]
 // uniqueInOrder(iterable)
+
+
+
+function comp(array1, array2){
+    if (array1 === null || array2 === null || array1.length !== array2.length) {
+      return false;
+    }
+
+    const squaredArray = array1.map(x => x ** 2);
+    const sorted1 = squaredArray.sort((a, b) => a - b);
+    const sorted2 = array2.sort((a, b) => a - b);
+
+    for (let i = 0; i < sorted1.length; i++) {
+      if (sorted1[i] !== sorted2[i]) {
+        console.log(false);
+        return false;
+      }
+    }
+    console.log(true);
+    return true;
+  }
+
+
+let array1 = [121, 144, 19, 161, 19, 144, 19, 11];
+let array2 = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+comp(array1, array2)
