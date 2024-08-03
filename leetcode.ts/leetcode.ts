7bcc2117bd7992fcbec04a1d3fad9385361e0765
Donald Powell <Djp0242@gmail.com>
2024-08-02 23:04:15 -0700

    leetcode

--- leetcode.ts/leetcode.ts
+++ leetcode.ts/leetcode.ts
@@ -450,3 +450,4 @@ console.log(minimumCost('abc', 'acd', ['a', 'b'], ['c', 'd'], [1, 2])); // -1
 console.log(minimumCost('xyz', 'xyz', [], [], [])); // 0
 console.log(minimumCost('abc', 'bcd', ['a', 'b', 'c'], ['b', 'c', 'd'], [1, 2, 3])); // 6
 console.log(minimumCost('abc', 'bcd', ['a', 'b', 'c'], ['b', 'c', 'd'], [1, 2, 3])); // 6
+console.log(minimumCost('abc', 'bcd', ['a', 'b', 'c'], ['b', 'c', 'd'], [1, 2, 3])); // 6
